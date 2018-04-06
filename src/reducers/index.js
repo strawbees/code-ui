2829c77f85908e2c5f60decc2aa508a591eba52c
Paulo Barcelos <paulo@azucrina.org>
2018-04-06 20:10:04 +0200

    Added redux for editor

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import setup from 'src/reducers/setup'
 import storage from 'src/reducers/storage'
 import qbmidi from 'src/reducers/qbmidi'
+import editor from 'src/reducers/editor'
 
 export default combineReducers({
 	setup,
 	qbmidi,
-	storage
+	storage,
+	editor
 })
