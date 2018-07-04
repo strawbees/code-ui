841a9d43a36be272c75626c5ffda86cc97b28822
Paulo Barcelos <paulo@azucrina.org>
2018-07-04 10:02:05 +0200

    linting

--- src/containers/pageContainer/index.js
+++ src/containers/pageContainer/index.js
@@ -1,29 +1,10 @@
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import dynamic from 'next/dynamic'
-import Spinner from 'src/components/spinner'
 import PageError from 'src/components/pageError'
 import PageHomeContainer from 'src/containers/pageHomeContainer'
 import PageFlowContainer from 'src/containers/pageFlowContainer'
 import PageBlockContainer from 'src/containers/pageBlockContainer'
 import PageTextContainer from 'src/containers/pageTextContainer'
-// const PageHomeContainer = dynamic(
-// 	import('src/containers/pageHomeContainer'),
-// 	{ loading : () => <Spinner/> }
-// )
-// const PageFlowContainer = dynamic(
-// 	import('src/containers/pageFlowContainer'),
-// 	{ loading : () => <Spinner/> }
-// )
-// const PageBlockContainer = dynamic(
-// 	import('src/containers/pageBlockContainer'),
-// 	{ loading : () => <Spinner/> }
-// )
-// const PageTextContainer = dynamic(
-// 	import('src/containers/pageTextContainer'),
-// 	{ loading : () => <Spinner/> }
-// )
-
 import mapStateToProps from './mapStateToProps'
 import mapDispatchToProps from './mapDispatchToProps'
 import mergeProps from './mergeProps'
