61fc49f6740090feaef044f4ba9be34e3accdcfd
Paulo Barcelos <paulo@azucrina.org>
2018-09-14 14:25:46 +0200

    Style

--- src/components/iconButton.js
+++ src/components/iconButton.js
@@ -65,11 +65,6 @@ const IconButton = ({
 		onClick={onClick}
 		className={`${className} iconButton`}
 		{...otherProps}>
-		<style jsx>{`
-			.iconButton {
-				min-height: 1.5rem;
-			}
-		`}</style>
 		<IconButtonContent
 			icon={icon}
 			label={label}
