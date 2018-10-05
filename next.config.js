cf0126825c37e43b88c3a4f257a78e7e7cbdb35f
Paulo Barcelos <paulo@azucrina.org>
2018-10-05 18:01:11 +0200

    Updated analytics

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const publicRuntimeConfig = {
 		CANONICAL_URL                 : 'http://code-dev.strawbees.com:3000',
 		COMPILER_URL                  : 'https://compiler.quirkbot.com',
 		STRAWBEES_CODE_API_URL        : 'https://api-stage.quirkbot.com',
-		GAID                          : 'UA-69443341-6',
+		GAID                          : 'XXXXXXXXXXXX',
 		CHROME_EXTENSION_ID           : 'ackaalhbfjagidmjlhlokoblhbnahegd',
 		WINDOWS_DRIVERS_INSTALLER_URL : 'https://github.com/Quirkbot/QuirkbotWindowsDriverInstaller/releases/download/2.0.0/quirkbot-windows-drivers-2.0.0.exe',
 	},
@@ -14,7 +14,7 @@ const publicRuntimeConfig = {
 		CANONICAL_URL                 : 'https://code-stage.strawbees.com',
 		COMPILER_URL                  : 'https://compiler.quirkbot.com',
 		STRAWBEES_CODE_API_URL        : 'https://api.quirkbot.com',
-		GAID                          : 'UA-69443341-6',
+		GAID                          : 'UA-69443341-7',
 		CHROME_EXTENSION_ID           : 'ackaalhbfjagidmjlhlokoblhbnahegd',
 		WINDOWS_DRIVERS_INSTALLER_URL : 'https://github.com/Quirkbot/QuirkbotWindowsDriverInstaller/releases/download/2.0.0/quirkbot-windows-drivers-2.0.0.exe',
 
