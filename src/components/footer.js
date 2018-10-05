faadf56c890e50cf073cd326d82bdf2d96030dee
Paulo Barcelos <paulo@azucrina.org>
2018-10-05 19:11:39 +0200

    Added footer links & copyright

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,21 @@
-import Package from 'root/package.json'
+import PropTypes from 'prop-types'
 import tinycolor from 'tinycolor2'
-import { PINK } from 'src/constants/colors'
+import Package from 'root/package.json'
+import Link from 'src/components/link'
 import BoardsStatusContainer from 'src/containers/boardsStatusContainer'
+import {
+	PINK,
+	WHITE,
+} from 'src/constants/colors'
 
-const Footer = () =>
+const Footer = ({
+	cookiePolicyTitle,
+	privacyPolicyTitle,
+	termsOfUseTitle,
+	cookiePolicyUrl,
+	privacyPolicyUrl,
+	termsOfUseUrl,
+}) =>
 	<div className='root footer'>
 		<style jsx>{`
 			.root {
@@ -17,16 +29,66 @@ const Footer = () =>
 			.root :global(>.boardsStatus) {
 				margin-left: -0.5rem;
 			}
+
+			.links {
+				flex: 1;
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+				justify-content: flex-end;
+			}
+			.links {
+				color: ${WHITE};
+				font-size: 0.7rem;
+				font-weight: bold;
+			}
+			.links :global(a){
+				text-decoration: none;
+			}
+			.links :global(>*){
+				margin-right: 0.5rem;
+			}
+			.links :global(>*:last-child){
+				margin-right: 0;
+			}
+			.links .copyright {
+				font-weight: normal;
+			}
 			.version {
 				position: absolute;
-				bottom: 2px;
-				right: 2px;
+				bottom: 0;
+				right: 1px;
 				font-family: Code;
 				font-size: 6px;
-				color: rgba(255,255,255,0.6);
+				color: rgba(255,255,255,0.3);
+			}
+			@media (max-width: 650px) {
+				.links {
+					font-size: 0.6rem;
+				}
+				.links :global(>*){
+					margin-right: 0.25rem;
+				}
+				.links .copyright {
+					display: none;
+				}
 			}
 		`}</style>
 		<BoardsStatusContainer />
+		<div className='links'>
+			<Link to={cookiePolicyUrl}>
+				{cookiePolicyTitle}
+			</Link>
+			<Link to={privacyPolicyUrl}>
+				{privacyPolicyTitle}
+			</Link>
+			<Link to={termsOfUseUrl}>
+				{termsOfUseTitle}
+			</Link>
+			<div className='copyright'>
+				© {(new Date()).getFullYear()} Strawbees AB
+			</div>
+		</div>
 		<div className='version'>
 			{Package.version}
 		</div>
@@ -34,6 +96,13 @@ const Footer = () =>
 
 Footer.defaultProps = {}
 
-Footer.propTypes = {}
+Footer.propTypes = {
+	cookiePolicyTitle  : PropTypes.string,
+	privacyPolicyTitle : PropTypes.string,
+	termsOfUseTitle    : PropTypes.string,
+	cookiePolicyUrl    : PropTypes.string,
+	privacyPolicyUrl   : PropTypes.string,
+	termsOfUseUrl      : PropTypes.string,
+}
 
 export default Footer
--- src/containers/footerContainer/mapStateToProps.js
+++ src/containers/footerContainer/mapStateToProps.js
@@ -1,1 +1,11 @@
-export default null
+import { createStructuredSelector } from 'reselect'
+import makeStringSelector from 'src/selectors/makeStringSelector'
+
+export default () => createStructuredSelector({
+	cookiePolicyTitle  : makeStringSelector('cookie-policy.base.title'),
+	privacyPolicyTitle : makeStringSelector('privacy-policy.base.title'),
+	termsOfUseTitle    : makeStringSelector('terms-of-use.base.title'),
+	cookiePolicyUrl    : makeStringSelector('cookie-policy.url'),
+	privacyPolicyUrl   : makeStringSelector('privacy-policy.url'),
+	termsOfUseUrl      : makeStringSelector('terms-of-use.url'),
+})
