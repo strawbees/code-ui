85dfa3fa5b1202ae53f6286ede13358bf9a2d6f1
Paulo Barcelos <paulo@azucrina.org>
2018-11-01 11:14:32 +0100

    Improving abortable fetch

--- src/utils/abortableFetch.js
+++ src/utils/abortableFetch.js
@@ -1,5 +1,13 @@
 export default (input, init = {}) => {
-	const controller = new AbortController()
+	let controller
+	if (typeof window.AbortController !== 'undefined') {
+		controller = new AbortController()
+	} else {
+		controller = {
+			abort  : () => throw new Error('AbortError'),
+			signal : () => {},
+		}
+	}
 	init.controller = controller
 	init.signal = controller.signal
 	return fetch(input, init)
