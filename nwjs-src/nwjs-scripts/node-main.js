99836eacf6359d757dd57f0788daf09f78306eba
Paulo Barcelos <paulo@azucrina.org>
2018-11-14 18:24:04 +0100

    Trying new compiler

--- nwjs-src/nwjs-scripts/node-main.js
+++ nwjs-src/nwjs-scripts/node-main.js
@@ -1,10 +1,16 @@
 const { fork } = require('child_process')
 
 // Start the compiler
-process.env.COMPILER_BUILD_ROOT = nw.App.dataPath
 process.env.COMPILER_PORT = 9511
 const compilerProcess = fork(
 	require.resolve('strawbees-code-compiler-service/app.js'),
-	{ stdio : 'pipe' }
+	[],
+	{ silent : true }
+)
+compilerProcess.stdout.on('data', data =>
+	console.log(`COMPILER stdout: ${data}`)
+)
+compilerProcess.stderr.on('data', data =>
+	console.log(`COMPILER stderr: ${data}`)
 )
 process.on('exit', () => compilerProcess.kill())
