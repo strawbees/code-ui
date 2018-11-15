b7ef29424e6b9a07cee66857ac30432d62b40d46
Paulo Barcelos <paulo@azucrina.org>
2018-11-15 17:06:13 +0100

    Updated compiler

--- nwjs-src/nwjs-scripts/inject_js_start.js
+++ nwjs-src/nwjs-scripts/inject_js_start.js
@@ -1,6 +1,7 @@
 const { fork } = require('child_process')
 
 // Start the compiler
+process.env.COMPILER_ROOT_DIR = window.nw.App.dataPath
 process.env.COMPILER_PORT = 9511
 const compilerProcess = fork(
 	require.resolve('strawbees-code-compiler-service/app.js'),
