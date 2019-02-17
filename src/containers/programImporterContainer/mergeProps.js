e5ea7c38d8df5acada4c03183eb81c40876c2a6e
Paulo Barcelos <paulo@azucrina.org>
2019-02-17 23:11:35 +0100

    fix bug introduced by dropzone update

--- src/containers/programImporterContainer/mergeProps.js
+++ src/containers/programImporterContainer/mergeProps.js
@@ -1,13 +1,1 @@
-export default (stateProps, dispatchProps, ownProps) => {
-	const {
-		modalImportProgram,
-		...otherDispatchProps
-	} = dispatchProps
-
-	return {
-		...stateProps,
-		...otherDispatchProps,
-		...ownProps,
-		onImportPress : () => modalImportProgram(),
-	}
-}
+export default null
