9a2afbcc2455415e81ac61928507a7fae43a1029
Paulo Barcelos <paulo@azucrina.org>
2019-02-18 12:56:15 +0100

    added new config versions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
-const routes = require('./static/routes')
-const locales = require('./static/locales/index')
+const routes = require('./static/routes.json')
+const locales = require('./static/locales/index.json')
 
 // load the correct publicRuntimeConfig, based on the COFING enviroment variable
 const configId = process.env.CONFIG || 'dev'
@@ -18,7 +18,7 @@ const configMap = {
 		WINDOWS_DRIVERS_INSTALLER_URL : 'https://github.com/Quirkbot/QuirkbotWindowsDriverInstaller/releases/download/2.0.0/quirkbot-windows-drivers-2.0.0.exe',
 		DOWNLOAD_DESKTOP_APP_URL      : 'https://s3.amazonaws.com/strawbees-downloads-stage/code-nwjs-build/versions',
 	},
-	stage : {
+	web_stage : {
 		CANONICAL_URL                 : 'https://code-stage.strawbees.com',
 		COMPILER_URL                  : 'https://compiler.quirkbot.com',
 		STRAWBEES_CODE_API_URL        : 'https://api.quirkbot.com',
@@ -29,7 +29,7 @@ const configMap = {
 		WINDOWS_DRIVERS_INSTALLER_URL : 'https://github.com/Quirkbot/QuirkbotWindowsDriverInstaller/releases/download/2.0.0/quirkbot-windows-drivers-2.0.0.exe',
 		DOWNLOAD_DESKTOP_APP_URL      : 'https://s3.amazonaws.com/strawbees-downloads-stage/code-nwjs-build/versions',
 	},
-	production : {
+	web_production : {
 		CANONICAL_URL                 : 'https://code.strawbees.com',
 		COMPILER_URL                  : 'https://compiler.quirkbot.com',
 		STRAWBEES_CODE_API_URL        : 'https://api.quirkbot.com',
@@ -40,13 +40,24 @@ const configMap = {
 		WINDOWS_DRIVERS_INSTALLER_URL : 'https://github.com/Quirkbot/QuirkbotWindowsDriverInstaller/releases/download/2.0.0/quirkbot-windows-drivers-2.0.0.exe',
 		DOWNLOAD_DESKTOP_APP_URL      : 'https://s3.amazonaws.com/strawbees-downloads-production/code-nwjs-build/versions',
 	},
-	desktop : {
-		CANONICAL_URL                 : 'https://code.strawbees.com',
+	desktop_stage : {
+		CANONICAL_URL                 : 'http://strawbeescode-stage',
+		COMPILER_URL                  : 'http://localhost:9511',
+		STRAWBEES_CODE_API_URL        : 'https://api-stage.quirkbot.com',
+		ROOT_PATH                     : 'ui',
+		URL_SCHEME                    : 'strawbeescode',
+		GAID                          : 'UA-69443341-7',
+		CHROME_EXTENSION_ID           : 'jgbaejhmonchgianepimdbcpfgcbdmam',
+		WINDOWS_DRIVERS_INSTALLER_URL : 'https://github.com/Quirkbot/QuirkbotWindowsDriverInstaller/releases/download/2.0.0/quirkbot-windows-drivers-2.0.0.exe',
+		DOWNLOAD_DESKTOP_APP_URL      : 'https://s3.amazonaws.com/strawbees-stage-production/code-nwjs-build/versions',
+	},
+	desktop_production : {
+		CANONICAL_URL                 : 'http://strawbeescode',
 		COMPILER_URL                  : 'http://localhost:9511',
 		STRAWBEES_CODE_API_URL        : 'https://api.quirkbot.com',
-		ROOT_PATH                     : '/node_modules/strawbees-code/out',
+		ROOT_PATH                     : 'ui',
 		URL_SCHEME                    : 'strawbeescode',
-		GAID                          : 'UA-69443341-9',
+		GAID                          : 'UA-69443341-8',
 		CHROME_EXTENSION_ID           : 'jgbaejhmonchgianepimdbcpfgcbdmam',
 		WINDOWS_DRIVERS_INSTALLER_URL : 'https://github.com/Quirkbot/QuirkbotWindowsDriverInstaller/releases/download/2.0.0/quirkbot-windows-drivers-2.0.0.exe',
 		DOWNLOAD_DESKTOP_APP_URL      : 'https://s3.amazonaws.com/strawbees-downloads-production/code-nwjs-build/versions',
