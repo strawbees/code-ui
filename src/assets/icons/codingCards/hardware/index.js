74738a263e1c91db44a2740d6e853b979ced7eec
Paulo Barcelos <paulo@azucrina.org>
2020-01-30 11:52:44 +0100

    updated coding cards hardware icons

--- src/assets/icons/codingCards/hardware/index.js
+++ src/assets/icons/codingCards/hardware/index.js
@@ -1,6 +1,7 @@
 import circuitTouch from './circuit-touch.svg'
 import dualColorLed from './dual-color-led.svg'
 import builtInLed from './built-in-led.svg'
+import keyPress from './key-press.svg'
 import lightSensor from './light-sensor.svg'
 import servoMotor from './servo-motor.svg'
 
@@ -8,6 +9,7 @@ export default {
 	'circuit-touch'  : circuitTouch,
 	'dual-color-led' : dualColorLed,
 	'built-in-led'   : builtInLed,
+	'key-press'      : keyPress,
 	'light-sensor'   : lightSensor,
 	'servo-motor'    : servoMotor,
 }
