211b93a220a4720f1f7929225124668e0ef3c0f4
Paulo Barcelos <paulo@azucrina.org>
2020-08-24 17:47:56 +0300

    Handling the new HYDRATE action fired by the next-react-redux-wrapper, that was causing the client state to be overitten.

--- src/store.js
+++ src/store.js
@@ -3,9 +3,23 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { HYDRATE, createWrapper } from 'next-redux-wrapper'
 import reducer from 'src/reducers/index'
 
+/**
+* Since the app is static only, we only need to hidrate it once. However, since
+* during development the app is partially server side, we need to include some
+* logic to persist the hydration on the server.
+*/
+let clientNeedsHydration = true
 const hidratableReducer = (state, action) => {
+	const isClient = typeof window !== 'undefined'
 	switch (action.type) {
 		case HYDRATE:
+			if (isClient) {
+				if (clientNeedsHydration) {
+					clientNeedsHydration = false
+					return { ...state, ...action.payload }
+				}
+				return state
+			}
 			return { ...state, ...action.payload }
 		default:
 			return reducer(state, action)
