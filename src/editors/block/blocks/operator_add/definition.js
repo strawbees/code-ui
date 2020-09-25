33ee987dc6527891d72000f55891ad4fc95a56ed
Paulo Barcelos <paulo@azucrina.org>
2020-09-25 18:38:10 +0200

    added number check

--- src/editors/block/blocks/operator_add/definition.js
+++ src/editors/block/blocks/operator_add/definition.js
@@ -1,3 +1,18 @@
-const definition = null
-
+const definition = () => ({
+	message0 : window.Blockly.Msg.OPERATORS_ADD,
+	args0    : [
+		{
+			type  : 'input_value',
+			name  : 'NUM1',
+			check : 'Number'
+		},
+		{
+			type  : 'input_value',
+			name  : 'NUM2',
+			check : 'Number'
+		}
+	],
+	category   : window.Blockly.Categories.operators,
+	extensions : ['colours_operators', 'output_number']
+})
 export default definition
--- src/editors/block/blocks/operator_and/definition.js
+++ src/editors/block/blocks/operator_and/definition.js
@@ -1,3 +1,18 @@
-const definition = null
-
+const definition = () => ({
+	message0 : window.Blockly.Msg.OPERATORS_AND,
+	args0    : [
+		{
+			type  : 'input_value',
+			name  : 'OPERAND1',
+			check : 'Boolean'
+		},
+		{
+			type  : 'input_value',
+			name  : 'OPERAND2',
+			check : 'Boolean'
+		}
+	],
+	category   : window.Blockly.Categories.operators,
+	extensions : ['colours_operators', 'output_boolean']
+})
 export default definition
