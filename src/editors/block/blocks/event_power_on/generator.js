ee55af0d0c7580ac9d340591b4593b41661de9fd
Paulo Barcelos <paulo@azucrina.org>
2020-10-06 22:27:59 +0200

    Renamed generated code for event_power_on

--- src/editors/block/blocks/event_power_on/generator.js
+++ src/editors/block/blocks/event_power_on/generator.js
@@ -6,7 +6,7 @@ import {
 } from '../../utils/parsing'
 
 const generator = ({ attributes, next }, structure) => {
-	const instance = computeInstanceName(structure, 'event_power_on', attributes.id)
+	const instance = computeInstanceName(structure, 'event_when_program_starts', attributes.id)
 	let body = ''
 	body += '// Code that runs when program starts:\n'
 	body += '{\n'
