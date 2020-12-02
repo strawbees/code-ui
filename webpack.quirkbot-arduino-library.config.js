e663241df21b613e3c501f957c866836c2e04e79
Paulo Barcelos <paulo@azucrina.org>
2020-12-02 16:46:44 +0100

    trying to resolve quirkbot-arduino-library build

--- webpack.quirkbot-arduino-library.config.js
+++ webpack.quirkbot-arduino-library.config.js
@@ -1,12 +1,18 @@
 const path = require('path')
 
 module.exports = {
+	resolveLoader : {
+		modules : [path.resolve(__dirname, 'node_modules')],
+	},
+	resolve : {
+		modules : [path.resolve(__dirname, 'node_modules')],
+	},
 	entry : [
 		'babel-polyfill',
-		'./src/simulator/quirkbotArduinoLibrary/Quirkbot.js'
+		path.resolve(__dirname, path.join('src', 'simulator', 'quirkbotArduinoLibrary', 'Quirkbot.js')),
 	],
 	output : {
-		path          : path.resolve(__dirname, 'static/lib/quirkbot-arduino-library'),
+		path          : path.resolve(__dirname, path.join('static', 'lib', 'quirkbot-arduino-library')),
 		library       : 'quirkbot-arduino-library',
 		libraryTarget : 'umd',
 		filename      : 'quirkbot-arduino-library.js',
@@ -18,7 +24,7 @@ module.exports = {
 					loader  : 'babel-loader',
 					options : {
 						presets : ['@babel/preset-env'],
-						plugins : ['@babel/plugin-proposal-class-properties']
+						plugins : ['@babel/plugin-proposal-class-properties'],
 					}
 				}
 			}
