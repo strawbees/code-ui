e322313462b5723255cf28e62e3644cf88e7c2be
Paulo Barcelos <paulo@azucrina.org>
2020-12-05 23:30:26 +0100

    created simulator-sandbox page managed by next

--- src/simulator/containers/simulatorVMManager/index.js
+++ src/simulator/containers/simulatorVMManager/index.js
@@ -10,6 +10,7 @@ import mergeProps from './mergeProps'
 
 const SimulatorVMManager = ({
 	rootPath,
+	simulatorSandboxUrl,
 	code,
 	externalData,
 	setInternalData,
@@ -37,223 +38,10 @@ const SimulatorVMManager = ({
 		iframeContainerRef.current = document.createElement('div')
 		iframeContainerRef.current.className = 'simulator-sandbox'
 		iframeContainerRef.current.style.display = 'none'
-		iframeContainerRef.current.innerHTML = `<iframe class='simulator-sandbox-iframe' sandbox="allow-scripts" src="data:text/html;charset=utf-8,${encodeURI(`
-			<script src="${window.location.origin}${rootPath}/static/lib/quirkbot-arduino-library/quirkbot-arduino-library.js"></script>
-			<script>
-				const programRef = {}
-				const loopTimerRef = {}
-				const externalDataRef = {}
-				const handleInternalDataTimerRef = {}
-				const mainWindow = {}
-
-				const setInternalData = (data) => {
-					mainWindow.source.postMessage({
-						key   : 'internalData',
-						value : data
-					}, mainWindow.origin)
-				}
-
-				const onMessage = (e) => {
-					const data = e.data
-					if (data.key === 'code') {
-						mainWindow.source = e.source
-						mainWindow.origin = e.origin
-						loadProgram(data.value)
-					} else if (data.key === 'data') {
-						externalDataRef.current = data.value
-					}
-				}
-
-				window.addEventListener('message', onMessage, false)
-
-				const loadProgram = (transpiledCode) => {
-					const cleanup = () => {
-						if (programRef.current) {
-							programRef.current.cancel()
-							programRef.current = null
-						}
-						if (loopTimerRef.current) {
-							clearTimeout(loopTimerRef.current)
-							loopTimerRef.current = null
-						}
-						if (handleInternalDataTimerRef.current) {
-							cancelAnimationFrame(handleInternalDataTimerRef.current)
-							handleInternalDataTimerRef.current = null
-						}
-					}
-					const start = async function() {
-						cleanup()
-						let Program
-						try {
-							const createProgramClass = (jsCode) => new Function(...Object.keys(window['quirkbot-arduino-library']), \`
-								const bootstrap = async () => {
-									/**
-									* Adaptations from the static C++ source to JS
-									* The Quirkbot C++ source uses static data - this would not
-									* allow us to run multiple instances of the simulator. So
-									* we overload certain variables with versions that we
-									* dynamicaly initialize.
-									**/
-									Bot = new Bot()
-									Node.Bot = Bot
-									const delay = createDelay(Bot)
-									const delayMicroseconds = createDelayMicroseconds(Bot)
-									const {
-										blockBegin,
-										blockEnd,
-										eventBegin,
-										eventEnd,
-										getBlockArg,
-										initEvent,
-										registerBlock,
-										registerEvent,
-										scheduleEvent,
-										wait,
-										waitUntil,
-										waitWhile,
-										spawnBlock,
-										yield,
-										yieldUntil,
-										threadBegin,
-										threadEnd,
-										THREAD,
-									} = new Protothreads(Bot)
-
-									/**
-									* Override globals
-									*/
-									const {
-										_babelPolyfill,addEventListener,alert,asap,atob,blur,btoa,cancelAnimationFrame,
-										cancelIdleCallback,captureEvents,chrome,clearImmediate,clearInterval,clearTimeout,
-										clientInformation,close,closed,confirm,core,createImageBitmap,crypto,customElements,
-										defaultstatus,defaultStatus,devicePixelRatio,dispatchEvent,document,external,fetch,
-										find,focus,frameElement,frames,getComputedStyle,getSelection,global,history,
-										indexedDB,innerHeight,innerWidth,isSecureContext,length,localStorage,location,
-										locationbar,matchMedia,menubar,moveBy,moveTo,name,navigator,Observable,onabort,
-										onafterprint,onanimationend,onanimationiteration,onanimationstart,onappinstalled,
-										onauxclick,onbeforeinstallprompt,onbeforeprint,onbeforeunload,onblur,oncancel,
-										oncanplay,oncanplaythrough,onchange,onclick,onclose,oncontextmenu,oncuechange,
-										ondblclick,ondrag,ondragend,ondragenter,ondragleave,ondragover,ondragstart,
-										ondrop,ondurationchange,onemptied,onended,onerror,onfocus,onformdata,
-										ongotpointercapture,onhashchange,oninput,oninvalid,onkeydown,onkeypress,onkeyup,
-										onlanguagechange,onload,onloadeddata,onloadedmetadata,onloadstart,
-										onlostpointercapture,onmessage,onmessageerror,onmousedown,onmouseenter,onmouseleave,
-										onmousemove,onmouseout,onmouseover,onmouseup,onmousewheel,onoffline,ononline,
-										onpagehide,onpageshow,onpause,onplay,onplaying,onpointercancel,onpointerdown,
-										onpointerenter,onpointerleave,onpointermove,onpointerout,onpointerover,onpointerrawupdate,
-										onpointerup,onpopstate,onprogress,onratechange,onrejectionhandled,onreset,onresize,
-										onscroll,onsearch,onseeked,onseeking,onselect,onselectionchange,onselectstart,onstalled,
-										onstorage,onsubmit,onsuspend,ontimeupdate,ontoggle,ontransitionend,onunhandledrejection,
-										onunload,onvolumechange,onwaiting,onwebkitanimationend,onwebkitanimationiteration,
-										onwebkitanimationstart,onwebkittransitionend,onwheel,open,openDatabase,opener,origin,
-										outerHeight,outerWidth,pageXOffset,pageYOffset,parent,performance,PERSISTENT,personalbar
-										,postMessage,print,prompt,queueMicrotask,regeneratorRuntime,releaseEvents,
-										requestAnimationFrame,requestIdleCallback,resizeBy,resizeTo,screen,screenLeft,screenTop,
-										screenX,screenY,scroll,scrollbars,scrollBy,scrollTo,scrollX,scrollY,self,sessionStorage,
-										setImmediate,setInterval,setTimeout,speechSynthesis,status,statusbar,stop,styleMedia,System,
-										TEMPORARY,toolbar,top,trustedTypes,visualViewport,webkitCancelAnimationFrame,
-										webkitRequestAnimationFrame,webkitRequestFileSystem,webkitResolveLocalFileSystemURL,
-										webkitStorageInfo,console} = {}
-
-									\${jsCode}
-
-									return {
-										cancel : () => {
-											delay.cancel()
-											delayMicroseconds.cancel()
-											cancelAllLoops()
-										},
-										setup : async () => {
-											await Bot.start()
-											await setup()
-											await Bot.afterStart()
-										},
-										loop : async () => {
-											await Bot.update()
-											await loop()
-										},
-										getInternalData : () => {
-											return Bot.getInternalData()
-										},
-										setExternalData : (data) => {
-											return Bot.setExternalData(data)
-										}
-									}
-								}
-								return bootstrap()
-							\`)
-							/* eslint-enable no-new-func */
-							Program = createProgramClass(transpiledCode)
-						} catch (e) {
-							console.groupCollapsed('Error creating program class')
-							console.log('Error:', e)
-							console.log('Transpiled code:', transpiledCode)
-							console.groupEnd()
-							// TODO: dispatch error action to signal the current't program is invalid
-							return
-						}
-
-						try {
-							programRef.current = await new Program(...Object.values(window['quirkbot-arduino-library']))
-						} catch (e) {
-							console.groupCollapsed('Error creating program instance')
-							console.log('Error:', e)
-							console.groupEnd()
-							// TODO: dispatch error action to signal the current't program is invalid
-							return
-						}
-
-						const handleInternalData = async () => {
-							try {
-								setInternalData(programRef.current.getInternalData())
-								if (externalDataRef.current) {
-									programRef.current.setExternalData(externalDataRef.current)
-								}
-							} catch (e) {
-								console.groupCollapsed('Error calling program.handleInternalData()')
-								console.log('Error:', e)
-								console.groupEnd()
-								// TODO: dispatch error action to signal the current't program crashed on loop
-								return
-							}
-							handleInternalDataTimerRef.current = requestAnimationFrame(handleInternalData, 0)
-						}
-						handleInternalData()
-
-						try {
-							await programRef.current.setup()
-						} catch (e) {
-							console.groupCollapsed('Error calling program.setup()')
-							console.log('Error:', e)
-							console.groupEnd()
-							// TODO: dispatch error action to signal the current't program crashed on setup
-							return
-						}
-
-						const loop = async () => {
-							try {
-								await programRef.current.loop()
-							} catch (e) {
-								console.groupCollapsed('Error calling program.loop()')
-								console.log('Error:', e)
-								console.log('This is likely an error in code inside loop(). See below.')
-								console.log('loop():', programRef.current?.loop)
-								console.log('Program:', Program)
-								console.groupEnd()
-								// TODO: dispatch error action to signal the current't program crashed on loop
-								return
-							}
-							loopTimerRef.current = setTimeout(loop, 0)
-						}
-						loop()
-					}
-					start()
-				}
-				</script>
-		`)}"/>`
+		iframeContainerRef.current.innerHTML = `<iframe class='simulator-sandbox-iframe' sandbox="allow-scripts ${process.env.NODE_ENV === 'development' ? 'allow-same-origin' : ''}" src="${simulatorSandboxUrl}"/>`
 		const iframe = iframeContainerRef.current.querySelector('.simulator-sandbox-iframe')
 		onMessageHandlerRef.current = (e) => {
-			if (e.origin !== 'null' || e.source !== iframe.contentWindow) {
+			if ((process.env.NODE_ENV !== 'development' && e.origin !== 'null') || e.source !== iframe.contentWindow) {
 				return
 			}
 			const { data } = e
@@ -320,10 +108,11 @@ const SimulatorVMManager = ({
 }
 
 SimulatorVMManager.propTypes = {
-	rootPath        : PropTypes.string,
-	code            : PropTypes.string,
-	externalData    : PropTypes.object,
-	setInternalData : PropTypes.func,
+	rootPath            : PropTypes.string,
+	simulatorSandboxUrl : PropTypes.string,
+	code                : PropTypes.string,
+	externalData        : PropTypes.object,
+	setInternalData     : PropTypes.func,
 }
 
 const SimulatorVMManagerConnected = connect(
